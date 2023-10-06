e22f1d2f49666fcb5ef40e0f23b676d0115aa946
Khang <khang06@users.noreply.github.com>
2023-10-06 13:53:17 -0400

    Bump version

--- PianoFromAbove/PianoFromAbove.cpp
+++ PianoFromAbove/PianoFromAbove.cpp
@@ -25,7 +25,7 @@
 #include "Misc.h"
 
 // Yes, I know you shouldn't store build numbers as doubles
-constexpr double BUILD_VERSION = 20231006;
+constexpr double BUILD_VERSION = 20231006.1;
 
 INT WINAPI WinMain( HINSTANCE hInstance, HINSTANCE hPrevInstance, LPSTR lpszCmdLine, INT nCmdShow );
 DWORD WINAPI GameThread( LPVOID lpParameter );
